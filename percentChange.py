92c06fd6ab2b9f757e0463c33872b4b451d0fba6
Devin Mix <63083435+Devin-Mix@user.noreply.github.com>
2021-04-12 09:02:00 -0400

    Fixed percentChange to include the DMIX. prefix in all table names

--- percentChange.py
+++ percentChange.py
@@ -6,25 +6,25 @@ def percentChange(currencies, startDate = datetime.date.min, endDate = datetime.
     start = dateToTimestamp(startDate)
     end = dateToTimestamp(endDate)
     
-    queryStr = 'SELECT {0}.DATE_TXT, '.format(currencies[0])
+    queryStr = 'SELECT DMIX.{0}.DATE_TXT, '.format(currencies[0])
     position = 0
     while position < len(currencies):
         if position != 0:
             queryStr = queryStr + ', '
-        queryStr = queryStr + '(({0}.CLOSE - {0}.OPEN) / {0}.OPEN)'.format(currencies[position])
+        queryStr = queryStr + '((DMIX.{0}.CLOSE - DMIX.{0}.OPEN) / DMIX.{0}.OPEN)'.format(currencies[position])
         position = position + 1;
     position = 0
     queryStr = queryStr + ' FROM '
     while position < len(currencies):
         if position == 0:
-            queryStr = queryStr + currencies[position]
+            queryStr = queryStr + "DMIX." + currencies[position]
         else:
-            queryStr = queryStr + ' INNER JOIN {1} ON {0}.DATE_TXT = {1}.DATE_TXT'.format(currencies[0], currencies[position])
+            queryStr = queryStr + ' INNER JOIN DMIX.{1} ON DMIX.{0}.DATE_TXT = DMIX.{1}.DATE_TXT'.format(currencies[0], currencies[position])
         position = position + 1
-    queryStr = queryStr + ' WHERE {0}.TIMESTAMP >= {1} AND {0}.TIMESTAMP <= {2}'.format(currencies[0], start, end)
+    queryStr = queryStr + ' WHERE DMIX.{0}.TIMESTAMP >= {1} AND DMIX.{0}.TIMESTAMP <= {2}'.format(currencies[0], start, end)
     for currency in currencies:
-        queryStr = queryStr + ' AND {0}.OPEN > 0'.format(currency)
-    queryStr = queryStr + ' ORDER BY {0}.DATE_TXT ASC'.format(currencies[0])
+        queryStr = queryStr + ' AND DMIX.{0}.OPEN > 0'.format(currency)
+    queryStr = queryStr + ' ORDER BY DMIX.{0}.DATE_TXT ASC'.format(currencies[0])
     
     headers = currencies
     headers.insert(0, "datetime")
