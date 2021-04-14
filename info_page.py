dd926967eaf6f828486de1ba3aeaeb2c6cb2074e
Ty87816 <44203626+Ty87816@users.noreply.github.com>
2021-04-14 16:35:22 -0400

    Update info_page.py
    Still need to fill in query summaries

--- info_page.py
+++ info_page.py
@@ -10,7 +10,28 @@ nav = navbar()
 def info_page():
     info = html.Div([
         nav,
-        html.P('This is the info page for our app, there will be instructions on how to use it as well as info about the data and where it came from')
+        html.H1("Information"),
+        html.H2("How to use:"),
+        html.P('    Each page is tied to a query. Use the navigation bar to expolre the different queries available.On each page, there will be a graph and a series of dropdown menus, slides, and other interactive functions. Choose the data you wish to see and wait to see how the graph changes in response.'),
+        html.H2("Queries:"),
+        html.P("Percent Change: Expolres blah"),
+        html.P("Currency Stability: Expolres blah"),
+        html.P("Currency Correlation: Expolres blah"),
+        html.P("Volatility: Expolres blah"),
+        html.P("Instability: Expolres blah"),
+        html.H2("Our Data:"),
+        html.P("Our data was collected from many places"),
+        html.Label(['Exchange Rates: ', html.A('International Monetary Fund', href='https://www.imf.org/external/np/fin/ert/GUI/Pages/CountryDataBase.aspx')]),
+        html.P(),
+        html.Label(['Crypto data(BAT, DASH, ETC, NEO, OMG, TRX) Bitfinex via: ', html.A('cryptodatadownload', href='cryptodatadownload.com')]),
+        html.P(),
+        html.Label(['Crypto data(XRP) Bitstamp via: ', html.A('cryptodatadownload', href='cryptodatadownload.com')]),
+        html.P(),
+        html.Label(['Crypto data(BTC, ETH, LTC, ZEC) Gemini via: ', html.A('cryptodatadownload', href='cryptodatadownload.com')]),
+        html.P(),
+        html.Label(['Education Spending, Literacy Rates, People On the Internet: ', html.A('Our World In Data', href='https://ourworldindata.org/')]),
+        html.P(),
+        html.Label(['Economic Uncertainty: ', html.A('Policy Uncertainty', href='https://www.policyuncertainty.com/all_country_data.html')])
     ])
     return info
 
